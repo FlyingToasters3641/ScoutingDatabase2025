cc6cf1d02679db3c2496f83ae974e7d28b06ed2c
ck2424 <chris.krozal@gmail.com>
2024-12-13 01:21:48 +0000

    Added Bootstraps, Menu, Api Call

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from 'axios';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navigationbar from "./components/Navigationbar.js";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Test from "./components/Test.js";
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
-
-  //const baseUrl = process.env.NEXT_STATIC_BASE_URL || "http://localhost:3001";
-  const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001/api'
-  
-  useEffect(() => {
-  //axios.get('https://jsonplaceholder.typicode.com/posts')
-  //axios.get('http://localhost:3001/users')
-  axios.get('https://super-sniffle-q4v55jpj9wcqrq-3001.app.github.dev/users')
-  .then(response => setPosts(response.data))
-  .catch(error => console.error('Error fetching data:', error));
-  }, []);
-  
   return (
-      <div className="App">
-          <h1>Name of the posts</h1>
-          <ul>
-          {posts.map(post => (
-              <li key={post.id}>{post.name} - {post.email}</li>
-          ))}
-          </ul>
-      </div>
+    <div className="App">
+        <Router>
+            <Navigationbar />
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/about' component={About}/>
+                <Route path='/test' component={Test}/>
+            </Switch>
+        </Router>
+    </div>
       );
   };
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,28 @@
+const Home = () => {
+    return (
+<>
+    <h1>The Flying Toaster's Scouting Database</h1>
+    <hr></hr>
+    <div className = "container">
+        <div className = "row">
+            <div className = "col">
+                <table className="table"> 
+                    <thead>
+                        <tr>
+                        <th><h2>Events</h2></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>List of Event here!</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</>
+    );
+}
+
+export default Home;
