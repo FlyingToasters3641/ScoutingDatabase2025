c7fcbf42e863f0e756da25e6064fb904a9e9f075
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2025-01-22 19:38:12 -0500

    Updated Eventimport.js
    - Updated Eventimport.js to add a link for The Blue Alliance

--- client/src/components/Eventimport.js
+++ client/src/components/Eventimport.js
@@ -12,7 +12,7 @@ const Eventimport = () => {
         <>
         <div className = "container">
             <h1>Event Import</h1>
-            <h3>Import an Event from The Blue Alliance using an Event Key:</h3>
+            <h3>Import an Event from <a href="https://www.thebluealliance.com/" target="_blank">The Blue Alliance</a> using an Event Key:</h3>
             <hr></hr>
             <form onSubmit={handleSubmit}>
                 <label>Event Key:
