b50f6181baa6fec0cf663504f5885f6f8cb58d69
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2025-01-25 15:24:08 -0500

    QR Code Scan Response
    - Updated Dataimport.js to display the contents of a scanned QR Code on to a page
    - Updated Dataimport.js to display to show the statis of scanned information

--- client/src/views/Matches/Dataimport.js
+++ client/src/views/Matches/Dataimport.js
@@ -3,10 +3,30 @@ import Html5QrcodePlugin from "../../components/Html5QrcodePlugin.js";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Dataimport = () => {
+
+    const [scannedData, setScannedData] = useState('');
+    const [scannedState, setScannedState] = useState('Waitting...');
+    // const [lastResult, setLastResult] = useState('');
+    // const [countResults, setCountResults] = useState('');
     
     const onNewScanResult = (decodedText, decodedResult) => {
-        alert(` Decoded Text: ${decodedText}`);
+        // if (decodedText !== lastResult) {
+        //     ++countResults;
+        // }
+        //alert(` Decoded Text: ${decodedText}`);
+        setScannedData(decodedText);
+        setScannedState('Success');
         console.log(`Scan result: ` + JSON.stringify(decodedResult) + `| ${decodedText}`);
+    };
+    
+    
+
+    const onScanSuccess = (decodedText, decodedResult) => {
+        //setScannedData(decodedText);
+    };
+
+    const onError = (error) => {
+    console.error(error);
     };
 
     return (
@@ -24,9 +44,15 @@ const Dataimport = () => {
                         fps={10}
                         qrbox={500}
                         disableFlip={false}
+                        onScanSuccess={onScanSuccess}
+                        onError={onError}
                         qrCodeSuccessCallback={onNewScanResult}
                     />
                 </Col>
+            </Row>
+            <Row>
+                <Col md={2}><h2>{ scannedState }</h2></Col>
+                <Col>{ scannedData }</Col>
             </Row>
         </Container>
     );
