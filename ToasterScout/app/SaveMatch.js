1a5f3ccecb84091abb7dbbadee9ce97ba61ac9b2
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2025-02-10 20:31:49 -0500

    Added Buttons to Save Matches
    - Added 'Generate QRCode' button
    - Added 'New Match' Button
    - Removed "This is content for Save Match" from 'Save Match' tab
    - Updated styles in 'SaveMatch.js' to include 'buttonText' style

--- ToasterScout/app/SaveMatch.js
+++ ToasterScout/app/SaveMatch.js
@@ -1,16 +1,64 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet } from 'react-native';
-
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 
 const SaveMatch = () => {
+  const [pressed1, setPressed1] = useState(false);
+
+  const handlePress = () => {
+    setPressed1(true);
+  };
+
+
     return (
-        <Text style={styles.contentText}>This is content for Save Match.</Text>
+      <>
+        <TouchableOpacity onPress={handlePress}>
+        <View
+          style={[
+            {
+              top: 100,
+              left: 25,
+              width: 200,
+              height: 100,
+              position: 'absolute',
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 10,
+            },
+          ]}
+        >
+          <Text style={styles.buttonText}>Generate QRCode</Text>
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress}>
+        <View
+          style={[
+            {
+              top: 275,
+              left: 25,
+              width: 200,
+              height: 100,
+              position: 'absolute',
+              backgroundColor: 'white',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 10,
+            },
+          ]}
+        >
+          <Text style={styles.buttonText}>New Match</Text>
+        </View>
+        </TouchableOpacity>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
   contentText: {
     fontSize: 22,
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
 
