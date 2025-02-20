a0ea0df2733111af7f7856ac0401ce867934baf0
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2025-02-20 17:12:29 -0500

    Changed Image and Added Buttons
    - Changed field imaged used on 'TeleOp' tab to a new, larger image of the reef
    - Added buttons for each section of the Reef
    - Added buttons to denote if a Coral was scored or not on the Reef
    - Added buttons for denoting when algae is scored in the Net or not
    - Added buttons for denoting when algae is scored in the Processor or not
    - Added buttons for Coral being picked up on the ground or Coral Station
    - Added a button for when Algae is picked up
    - Changed the background of 'TeleOp' from Orange to Black

--- ToasterScout/app/TeleOp.js
+++ ToasterScout/app/TeleOp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Text, StyleSheet, View, Image, TouchableOpacity, Alert } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons'; //https://icons.expo.fyi/Index
-
+import Entypo from '@expo/vector-icons/Entypo';
 
 const TeleOp = () => {
 
@@ -44,62 +44,308 @@ const TeleOp = () => {
           width: 906,
           height: 477,
           position: 'absolute',
-          backgroundColor: 'powderblue',
+          backgroundColor: 'black',
         },
       ]}>
-        <Image
-          source={require('@/assets/images/g2025-fieldview-blue.png')} 
-          style={{ width: 524, height: 477 }} 
-        />
     </View>
-    <View style={[
-        {
-          top: 100,
-          left: 100,
-          width: 50,
-          height: 50,
-          position: 'absolute',
-          backgroundColor: 'powderblue',
-          justifyContent: 'center',
-          alignItems: 'center',
-        },
-      ]}
-    >
+    <View>
+      <Image
+        source={require('@/assets/images/g2025-fieldview-teleop-blue.png')} 
+        style={{ left: 10, width: 412, height: 470 }} 
+      />
       <TouchableOpacity
-            activeOpacity={0.5}
-            key="plus1"
-            onPress={showAlert}>
-            <Ionicons name="add" size={32} color="white" />
-          </TouchableOpacity>
-    </View>
-    <View style={[
-        {
-          top: 100,
-          left: 300,
-          width: 50,
-          height: 50,
-          position: 'absolute',
-          backgroundColor: 'powderblue',
-          justifyContent: 'center',
-          alignItems: 'center',
-        },
-      ]}
-    >
+        style={[
+          styles.smallButton,
+          {
+            top: 115,
+            left: 86,
+          },
+        ]}
+        activeOpacity={0.5}
+        key="plus1"
+        onPress={showAlert}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
       <TouchableOpacity
-            activeOpacity={0.5}
-            key="plus2"
-            onPress={createTwoButtonAlert}>
-            <Ionicons name="add" size={32} color="white" />
-          </TouchableOpacity>
+        style={[
+          styles.smallButton,
+          {
+            top: 80,
+            left: 155,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 80,
+            left: 218,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 115,
+            left: 286,
+          }
+        ]}
+        activeOpacity={0.5}
+        key="plus2"
+        onPress={createTwoButtonAlert}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 172,
+            left: 315,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 243,
+            left: 315,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 295,
+            left: 286,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 331,
+            left: 218,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 331,
+            left: 162,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 295,
+            left: 90,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 243,
+            left: 65,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.smallButton,
+          {
+            top: 169,
+            left: 65,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle" size={24} color="black" />
+      </TouchableOpacity>
     </View>
-    </>
 
-    );
+    <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 10,
+            left: 490,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-plus" size={30} color="black" /><Text style={styles.bigButtonText}>Coral Scored</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 10,
+            left: 685,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-cross" size={30} color="black" /><Text style={styles.bigButtonText}>Coral Missed</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 110,
+            left: 490,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-plus" size={30} color="black" /><Text style={styles.bigButtonText}>Net Scored</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 110,
+            left: 685,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-cross" size={30} color="black" /><Text style={styles.bigButtonText}>Net Missed</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 210,
+            left: 490,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-plus" size={30} color="black" /><Text style={styles.bigButtonText}>Processor Scored</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.bigButton,
+          {
+            top: 210,
+            left: 685,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Entypo name="circle-with-cross" size={30} color="black" /><Text style={styles.bigButtonText}>Processor Missed</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.midButton,
+          {
+            top: 360,
+            left: 490,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Text style={styles.midButtonText}>Coral Ground Pick-Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.midButton,
+          {
+            top: 360,
+            left: 620,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Text style={styles.midButtonText}>Coral Station Pick-Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.midButton,
+          {
+            top: 360,
+            left: 750,
+          },
+        ]}
+        activeOpacity={0.5}>
+        <Text style={styles.midButtonText}>Algae Pick-Up</Text>
+      </TouchableOpacity>
+  </>
+
+  );
 }
 
 const styles = StyleSheet.create({
   contentText: {
     fontSize: 22,
+  },
+  midButtonText: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  bigButtonText: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  smallButton: {
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'oldlace'
+  },
+  bigButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 8,
+    marginTop: 8,
+    marginBottom: 8,
+    width: 182,
+    height: 75,
+    position: 'absolute',
+    backgroundColor: 'oldlace',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  midButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 8,
+    marginTop: 8,
+    marginBottom: 8,
+    width: 115,
+    height: 75,
+    position: 'absolute',
+    backgroundColor: 'oldlace',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   },
 });
 
