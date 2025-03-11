386a738be19607f2b52c2098dbab59142ef597db
ck2424 <chris.krozal@gmail.com>
2025-03-11 14:24:47 -0400

    - Updated the home view to update the appData for an event before displaying the Event Details view. - Updated AppContext to recall appData on reload and save on change of appData - Updated Event Data view  to use a fluid container for the large table.

--- client/src/views/common/AppContext.js
+++ client/src/views/common/AppContext.js
@@ -1,11 +1,20 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 // Create a context
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  
+  const defaultAppData = {name: 'Default', isLoggedIn: true, currentEventID: 0, currentEventKey: '', currentEventYear: 0};
+  const initialAppData = JSON.parse(localStorage.getItem('appData')) || defaultAppData;
+  const [appData, setAppData] = useState(initialAppData);
+
+  // Update localStorage whenever appData changes
+  useEffect(() => {
+    localStorage.setItem('appData', JSON.stringify(appData));
+  }, [appData]);
+
   const [theme, setTheme] = useState('light');
-  const [appData, setAppData] = useState({ name: 'AppData is working but hardcoded... Please Fix It!', isLoggedIn: true, currentEventID: 1, currentEventKey: '2025mimil', currentEventYear: 2025 });
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
