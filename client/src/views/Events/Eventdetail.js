957a331058edcf241d00a80e8ff24198ab396ee4
Jacobk12345 <152231698+Jacobk12345@users.noreply.github.com>
2025-03-11 20:54:52 -0400

    Add modal to Eventdetails for adding teams (does not work)

--- client/src/views/Events/Eventdetail.js
+++ client/src/views/Events/Eventdetail.js
@@ -2,18 +2,30 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from 'axios';
 import { useLocation, Link, useHistory } from 'react-router-dom';
 import { AppContext } from "../common/AppContext.js";
-import { Container, Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Container, Row, Col, OverlayTrigger, Tooltip, Modal } from "react-bootstrap";
 import { APP_DATABASE_URL } from "../../constant/constant";
 import BackButton from '../common/BackButton';
 import { RiTrophyLine, RiAddCircleLine } from "react-icons/ri";
 import { MdOutlinePreview } from "react-icons/md";
 import DataTable from '../../components/DataTableNetBase.js';
+import { Dropdown } from 'primereact/dropdown';
+import { count } from "../../../../server/models/matchData2024.js";
 
 const Eventdetail = () => {
     const { appData } = useContext(AppContext);
     const [event, setEvent] = useState([]);
     const [team, setTeam] = useState([]);
     const [match, setMatch] = useState([]);
+    
+    const [showAddTeam, setShowAddTeam] = useState(false);
+    const [selectedTeam, setSelectedTeam] = useState([]);
+    const [allTeams, setAllTeams] = useState([]);
+    const [teamnumber, setTeamnumber] = useState('');
+    const [teamnickname, setTeamnickname] = useState('');
+    const [teamcity, setTeamcity] = useState('');
+    const [teamstateprov, setTeamstateprov] = useState('');
+    const [teamcountry, setTeamcountry] = useState('');
+
     const location = useLocation();
     const history = useHistory();
     const searchParams = new URLSearchParams(location.search);
@@ -31,6 +43,10 @@ const Eventdetail = () => {
         axios.get(`${APP_DATABASE_URL}/matches/${eventid}`)
         .then(response => setMatch(response.data))
         .catch(error => console.error('Error fetching data:', error));
+
+        axios.get(`${APP_DATABASE_URL}/teams`)
+        .then(response => setAllTeams(response.data))
+        .catch(error => console.error('Error fetching data:', error));
     }, [eventid]);
 
     const tdRight = {
@@ -42,6 +58,10 @@ const Eventdetail = () => {
     };
 
     const renderTooltip = (props) => (<Tooltip {...props}>{props.text}</Tooltip>);
+
+    async function addTeam (team) {
+        team.preventDefault();
+    }
 
     return (
         <Container>
@@ -123,6 +143,77 @@ const Eventdetail = () => {
                 </Col>
                 <Col md={5}>
                     <h2>Team List:</h2>
+                    <button onClick={() => setShowAddTeam(true)} className="btn btn-success">
+                        <RiAddCircleLine /> Add Team
+                    </button>
+                    <Modal
+                        size="lg"
+                        show={showAddTeam}
+                        backdrop="static"
+                        onHide={() => showAddTeam(false)}
+                    >
+                        <Modal.Header>Add Team</Modal.Header>
+                        <Modal.Body>
+                        {/* <form onSubmit={addTeam}>
+                            <label>Team Number:
+                                <input 
+                                    type="text" 
+                                    value={teamnumber}
+                                    onChange={(e) => setTeamnumber(e.target.value)}
+                                />
+                            </label>
+                            <label>Team Name:
+                                <input 
+                                    type="text" 
+                                    value={teamnickname}
+                                    onChange={(e) => setTeamnickname(e.target.value)}
+                                />
+                            </label>
+                            <label>Team City:
+                                <input 
+                                    type="text" 
+                                    value={teamcity}
+                                    onChange={(e) => setTeamcity(e.target.value)}
+                                />
+                            </label>
+                            <label>Team State/Province:
+                                <input 
+                                    type="text" 
+                                    value={teamstateprov}
+                                    onChange={(e) => setTeamstateprov(e.target.value)}
+                                />
+                            </label>
+                            <label>Team City:
+                                <input 
+                                    type="text" 
+                                    value={teamcountry}
+                                    onChange={(e) => setTeamcountry(e.target.value)}
+                                />
+                            </label>
+                            <input className="btn btn-primary" type="submit" value="Submit"/>
+                        </form> */}
+                        </Modal.Body>
+
+                        {/* <Modal.Body>   
+                        <Dropdown 
+                            value={selectedTeam} 
+                            onChange={(e) => setSelectedTeam(e.value)} 
+                            options={allTeams} 
+                            optionLabel="name" 
+                            placeholder="Select a Team" 
+                            className="w-full md:w-14rem" 
+                        />
+                        </Modal.Body> */}
+                        <Modal.Footer>
+                            <button className="btn btn-primary" onClick={() => setShowAddTeam(false)}>
+                            Close
+                            </button>
+                            <button className="btn btn-success" onClick={() => setShowAddTeam(false)}>
+                            Save
+                            </button>
+                        </Modal.Footer>
+                    </Modal>
+                    
                     <DataTable
                         data={team}
                         options={{
